7ae2ad47022859e9590bba13fec92a9d6f17a6aa
Mahad Siad <mahad.siad@interactivesoftware.co.uk>
2024-07-21 16:34:56 +0100

    Added email and phone validation

--- CustomerDataProcessingApp.Web/Models/Customer.cs
+++ CustomerDataProcessingApp.Web/Models/Customer.cs
@@ -1,10 +1,14 @@
-﻿namespace CustomerDataProcessingApp.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerDataProcessingApp.Web.Models
 {
     public class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+        [Phone]
         public string Phone { get; set; }
 
     }
